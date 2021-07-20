0a0c9f0e2143cdc3109ded9043bc954f1ef7f808
ASguoyue <1297707739@qq.com>
2021-07-20 11:06:16 +0800

    ASguoyue/ctest

---
+++ test.cpp
@@ -0,0 +1,19 @@
+// 这是一个调试测试文件，您可以直接运行检测配置是否生效
+// 若配置任然无法调试，请查阅README_inProject.md
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //printf("Congratulasions! It works!\n");
+    
+
+    for(int i = 0; i < 10; i++)
+    {
+       int res = i*i;
+       
+         cout << i << endl;
+
+    }
+    
+}
