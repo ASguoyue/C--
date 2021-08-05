a7548ded6466e12be5521820cf59e9dac7d6c46f
ASguoyue <1297707739@qq.com>
2021-08-06 00:10:28 +0800

    master

--- 算法/swap_point.cpp
+++ 算法/swap_point.cpp
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[])
     int *q = &b;
     swap(p,q);
     printf("%d %d ",*p,*q);  //只能通过指针获取后才进行了改变，应为传入的参数是指针类型
-    printf("%d %d \n",&a,p);  //通过打印a b两个变量不会发生改变，应为主函数定义的值
+    printf("%d %d \n",&a,p);  //通过打印a b两个变量不会发生改变，应为交换的是指针变量，主函数定义的值
     printf("%d %d",&b,q);
     return 0;
 }
