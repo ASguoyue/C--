ca793fb78e097acfcbdf26214b348afa100f143e
ASguoyue <1297707739@qq.com>
2021-08-05 16:44:43 +0800

    master

--- learn_point.cpp
+++ learn_point.cpp
@@ -4,12 +4,12 @@ int main(int argc, char const *argv[])
 {
     int a;
     int *p = &a;
-    a = 233;
+    a = 233;  //等价语句 *p = 233，a是变量，p是指针，代表地址，*p代表便量
     printf("%d  %d",&a,a);
     return 0;
 
 
 
 
-    
+
 } 
---
+++ learn_point_arry.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a[5]={1,2,3,4,5};
+    //定义指针变量，并且指针变量支持自增操作，for循环中的写法
+    //a代表数组，数组的名称也做为数组的首地址按使用,&a[0]
+    for (int *i = a; i < a+5; i++)
+    {
+        /* code */
+        printf("%d ",*i); //取指针的元素使用*p
+    }
+    printf("\n");
+    //定义指针变量的减法，两个指针相减，相当于中间的变量的个数，不是地址的相减
+    int *p = a;
+    int *q = &a[4];
+
+    printf("%d \n",p);
+    printf("%d \n",q);
+    printf("p-q:%d\n",q-p); //指针计算的单位使用的是变量类型int,不是byte
+    
+    
+    return 0;
+}
